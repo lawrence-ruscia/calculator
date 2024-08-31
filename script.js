fd9ca4419007dd1ecfe0ac7ba8a82a76a4809609
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2024-08-31 21:19:53 +0800

    feat: add feature that appends each value when key is pressed

---
+++ script.js
@@ -0,0 +1,17 @@
+// add a click event to target key
+// append each key clicked on display
+const keypad = document.querySelector(".calculator-keypad");
+
+keypad.addEventListener("click", (e) => {
+  if (e.target.classList.contains("numeric")) {
+    const value = e.target.textContent;
+
+    appendToDisplay(value);
+  }
+});
+
+function appendToDisplay(value) {
+  const currentOperator = document.querySelector(".current-operator");
+
+  currentOperator.textContent += value;
+}
