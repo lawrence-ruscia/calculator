1aae4cbb7cbf811b31234bc03807daf20db77106
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2024-09-01 10:59:11 +0800

    bugfix: current and previous operand now have correct values when user inputs numbers
    This fixes the bug where previously, the value is still appended to the currentOperand, even after an operator is selected, which should be only the previousOperand

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const keypad = document.querySelector(".calculator-keypad");
 
-const previousOperand = document.querySelector(".previous-operand");
 const currentOperand = document.querySelector(".current-operand");
+const previousOperand = document.querySelector(".previous-operand");
+
+const operation = {
+  currentOperand: "",
+  operator: "",
+  previousOperand: "",
+};
 
 keypad.addEventListener("click", (e) => {
   if (e.target.classList.contains("numeric")) {
@@ -18,13 +24,14 @@ keypad.addEventListener("click", (e) => {
 });
 
 function appendToDisplay(value) {
-  const currentOperand = document.querySelector(".current-operand");
-
   if (currentOperand.textContent === "0") {
     currentOperand.textContent = "";
   }
 
   if (isOnlyOneDecimal(currentOperand.textContent, value)) {
+    operation.currentOperand += value;
+
+    console.log("current: " + operation.currentOperand);
     currentOperand.textContent += value;
   }
 }
@@ -42,15 +49,19 @@ function handleNumericKeys(e) {
 function handleOperatorKeys(e) {
   const operator = e.target.textContent;
   operation.operator = operator;
+  console.log("operator: " + operator);
 
   const currentTextContent = currentOperand.textContent;
-
   currentOperand.textContent = "";
+  operation.currentOperand = ""; // clear current operand for new input
 
   previousOperand.textContent = `${currentTextContent} ${operator}`;
+  operation.previousOperand += currentTextContent; // store current operand to previous
+  console.log("previous: " + operation.previousOperand);
 }
 
 // TODO: Create functionality for equals button,
 // it should do the operation on previousOperand & currentOperand after clicked
 
-function handleEqualsKey(e) {}
+// Track previous operand, operator, and current operand
+// create a function operate() to perform the operation based on the operator
